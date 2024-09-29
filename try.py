3cb4dfca9015f4b87cff4f590915854102f92a45
Makoo <141389899+makuu12@users.noreply.github.com>
2024-09-29 17:33:30 +0800

    add new main

---
+++ try.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from lib.driver_init import init_browser
+from lib.take_ss import take_screenshot
+
+import time
+
+init_browser()
+driver = webdriver.Chrome()
+
+screenshot_path = take_screenshot(driver, folder_path = "Downloads", file_name="test_screenshot.png")
+print("Screenshot has been captured")
