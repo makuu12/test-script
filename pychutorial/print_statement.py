902b3d34087f325e2d244ea6dace28922c896473
Makoo <141389899+makuu12@users.noreply.github.com>
2024-09-30 21:18:23 +0800

    add

---
+++ pychutorial/print_statement.py
@@ -0,0 +1,31 @@
+# ------------------ COMMENT ------------------ #
+
+# this is one line comment
+'''
+This is a multiline comment
+This is a multiline comment
+This is a multiline comment
+This is a multiline comment
+This is a multiline comment
+
+'''
+
+# ------------------ PRINT STATEMENT ------------------ #
+
+print("Hello world! 1") # Generic print method
+greetings = "Hello world! 2" # Print using variable
+print(greetings)
+
+# ------------------ PRINT WITH CONCATENATION & SEPERATOR ------------------ #
+
+line1 = "hell"
+line2 = "o"
+# with concatenationx
+print(line1+line2) # 2 combined string without a space
+# with seperation
+print(line1,line2) # 2 combined string with a space
+
+first_name = "Tite"
+last_name = "Kubo"
+
+print("Hello, my name is "+ first_name, last_name, ". I am the author of bleach")
