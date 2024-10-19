f26dc8faf16bddad5f49ff1ba95c88ae3920e425
Makoo <141389899+makuu12@users.noreply.github.com>
2024-10-19 17:27:15 +0800

    addsome

---
+++ pychutorial/operators.py
@@ -0,0 +1,14 @@
+# ------------------ PYTHON AS A CALCULATOR ------------------ #
+
+print(1+1) # Output: 2 (Addition)
+print(1-1) # Output: 0 (Subtraction)
+print(1*1) # Output: 1 (Multiplication)
+print(1/1) # Output: 1.0 (Division)
+print(1//1) # Output: 1 (Floor Division)
+print(1%1) # Output: 0 (Modulus)
+print(2**3) # Output: 8 (Exponentiation)
+
+# ------------------ PRINT using 'end' PARAMETER ------------------ #
+
+
+# ------------------ PRINT using 'end' PARAMETER ------------------ #
